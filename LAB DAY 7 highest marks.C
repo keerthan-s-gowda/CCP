d395e53a2a8576e0a17d3b07982efe2310b89ff6
keerthan-s-gowda <85472308+keerthan-s-gowda@users.noreply.github.com>
2021-06-28 10:32:50 +0530

    Add files via upload

---
+++ LAB DAY 7 highest marks.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int marks[5][3],i,j,max_marks;
+ clrscr();
+ for(i=0;i<5;i++)
+ {
+  printf("\n enter the marks of student %d:",i);
+  for(j=0;j<3;j++)
+  {
+  printf("\t marks[%d][%d]=",i,j);
+  scanf("%d",&marks[i][j]);
+  }
+ }
+ for(j=0;j<3;j++)
+ {
+   max_marks=marks[0][j];
+   for(i=0;i<5;i++)
+   if(marks[i][j]>max_marks)
+   {
+     max_marks=marks[i][j];
+   }
+  printf("\n the highest marks obtained in the subject%d=%d",j,max_marks);
+  }
+  getch();
+  return(0);
+}
